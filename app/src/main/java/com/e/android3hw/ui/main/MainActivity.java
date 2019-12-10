e38cfd422ce8f1c0860f2e69566cbdd3c29d5b41
Malikovich <md.malikovich@gmail.com>
2019-12-10 19:13:02 +0600

    1

---
+++ app/src/main/java/com/e/android3hw/ui/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.e.android3hw.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.e.android3hw.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
