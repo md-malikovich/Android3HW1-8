e8b8ec2489d701a9f6f7346c1f571d2bcc6a6809
Malikovich <md.malikovich@gmail.com>
2019-12-10 20:29:41 +0600

    3

--- app/src/main/java/com/e/android3hw/ui/main/MainActivity.java
+++ app/src/main/java/com/e/android3hw/ui/main/MainActivity.java
@@ -1,10 +1,11 @@
 package com.e.android3hw.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-
 import com.e.android3hw.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static void start(Context context) {
+        context.startActivity(new Intent(context, MainActivity.class));
     }
 }
--- app/src/main/java/com/e/android3hw/ui/splash/SplashActivity.java
+++ app/src/main/java/com/e/android3hw/ui/splash/SplashActivity.java
@@ -1,19 +1,32 @@
 package com.e.android3hw.ui.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
 import com.e.android3hw.R;
+import com.e.android3hw.ui.main.MainActivity;
+import com.e.android3hw.ui.onboard.OnBoardActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private Boolean isShown = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        selectActivity();
+    }
 
+    private void selectActivity() {
         SharedPreferences preferences = getSharedPreferences("pref", MODE_PRIVATE);
+        isShown = preferences.getBoolean("isFirstLaunch", false);
+
+        if(isShown) {
+            MainActivity.start(this);
+        } else {
+            preferences.edit().putBoolean("isFirstLaunch", true).apply();
+            OnBoardActivity.start(this);
+        }
     }
 }
