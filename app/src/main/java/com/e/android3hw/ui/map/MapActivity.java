3495942ddc8640a5afcb06e6dc356af1ce23113b
Malikovich <md.malikovich@gmail.com>
2019-12-23 16:51:58 +0600

    +получение данных при нажатии

--- app/src/main/java/com/e/android3hw/ui/map/MapActivity.java
+++ app/src/main/java/com/e/android3hw/ui/map/MapActivity.java
@@ -3,9 +3,15 @@ package com.e.android3hw.ui.map;
 import androidx.annotation.NonNull;
 import androidx.annotation.UiThread;
 
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 import com.e.android3hw.R;
 import com.e.android3hw.ui.base.BaseActivity;
+import com.google.gson.JsonElement;
+import com.mapbox.geojson.Feature;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
@@ -22,6 +28,11 @@ import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager;
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions;
 import com.mapbox.mapboxsdk.utils.BitmapUtils;
 
+import org.w3c.dom.Text;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
 
 import static com.e.android3hw.BuildConfig.MAPBOX_KEY;
@@ -33,9 +44,10 @@ public class MapActivity extends BaseActivity implements OnMapReadyCallback {
     private MapboxMap map;
     SymbolManager symbolManager;
     private Symbol symbol;
+    private TextView tvMap;
 
-    private static final LatLng locationFirst = new LatLng(44.000, 55.000);
-    private static final LatLng locationSecond = new LatLng(45.000, 56.000);
+//    private static final LatLng locationFirst = new LatLng(34.035619, -118.224380);
+//    private static final LatLng locationSecond = new LatLng(33.986300, -118.184196);
 
     @Override
     protected int getLayoutId() {
@@ -72,16 +84,35 @@ public class MapActivity extends BaseActivity implements OnMapReadyCallback {
             map.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
                 @Override
                 public boolean onMapClick(@NonNull LatLng point) {
-                    LatLngBounds latLngBounds = new LatLngBounds.Builder()
-                            .include(locationFirst)
-                            .include(locationSecond)
-                            .build();
-                    createSymbol(point);
+//                    LatLngBounds latLngBounds = new LatLngBounds.Builder()
+//                            .include(locationFirst)
+//                            .include(locationSecond)
+//                            .build();
+//                    createSymbol(point);
+
+                    PointF pixel = map.getProjection().toScreenLocation(point);
+                    List<Feature> features = map.queryRenderedFeatures(pixel, LatLng.CREATOR.toString());
+                    if (features.size() > 0) {
+                        Feature feature = features.get(0);
+
+                        if (feature.properties() != null) {
+                            for (Map.Entry<String, JsonElement> entry : feature.properties().entrySet()) {
+                                Log.d("ololo", String.format("%s = %s", entry.getKey(), entry.getValue()));
+                            }
+                        }
+                    }
                     return true;
                 }
             });
         });
+
     }
+
+//    @Override
+//    public void onMapClick(@NonNull LatLng point) {
+//
+//
+//    }
 
     private void createSymbol(LatLng latLng) {
         SymbolOptions symbolOptions = new SymbolOptions()
@@ -91,7 +122,7 @@ public class MapActivity extends BaseActivity implements OnMapReadyCallback {
         symbol = symbolManager.create(symbolOptions);
 
         map.getStyle().addImageAsync("icon", Objects.requireNonNull(BitmapUtils.getBitmapFromDrawable(
-                getResources().getDrawable(R.drawable.ic_location))));
+                getResources().getDrawable(R.drawable.ic_place_black_24dp))));
     }
 
     @Override
